bf5f64a2a2e8fdbd0cc0081196a457d32b831364
Cunov <nevs.servicedesk@dxc.com>
2023-03-01 10:39:33 +0100

    linting

--- annotation_converter/annotation_converter/__init__.py
+++ annotation_converter/annotation_converter/__init__.py
@@ -4,15 +4,11 @@
 # Futures
 
 # Built-in/Generic Imports
-# import os
-# import sys
+
 # Libs
 
 # Own modules
-from . import module
 from .converter import convert
-# from .converter import convert
-# from . import params
 
 __author__ = 'Colton Cunov'
 __email__ = 'colton.cunov@nevs.com'
@@ -21,24 +17,3 @@ __credits__ = '{{ credit_list }}'
 __version__ = '0.0.1'
 __maintainer__ = 'Colton Cunov'
 __status__ = 'Under Dev'
-
-# HOST = '127.0.0.1'
-# PORT = 5000
-# NUM_TRIES = 5
-# BASE_URL = f'http://{HOST}:{PORT}/'
-# TEMPLATE_FILE = os.path.join(os.path.dirname(__file__), 'application', 'open_label.txt')
-
-# class This(sys.__class__):
-#     _host = HOST
-#     _port = PORT
-#     _num_tries = NUM_TRIES
-#     _base_url = BASE_URL
-#     _template_file = TEMPLATE_FILE
-
-#     @property
-#     def host(self):
-#         return self._host
-    
-#     @host.setter
-#     def host(self, host):
-#         self._host = host
